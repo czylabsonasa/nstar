258ffd78ab59e77e388a959269d9e3498da48ed5
Noszály Csaba <czylabson.asa@gmail.com>
2018-09-18 15:38:38 +0200

    progress

--- Main.jl
+++ Main.jl
@@ -15,7 +15,7 @@ Stati.Init(Nstar.maxL)
 # uncomment the next row and adjust the parameter to get a fixed graph
 # srand( 1080 )
 
-println("nV=",Nstar.nV)
+# println("nV=",Nstar.nV)
 
 for rep in 1:Sim.ism
    println( "-----------------\n" )
@@ -23,7 +23,8 @@ for rep in 1:Sim.ism
    psiz=1
    for siz in Sim.chkpts
       Nstar.Step(psiz,siz) # 
-      Stati.proc(Nstar.W,Nstar.nV,siz)
+      Stati.proc(Nstar.W,Nstar.nV,siz,1)
+      Stati.proc(Nstar.W,Nstar.nV,siz,2)
       psiz=siz
    end
 end
--- Stati.jl
+++ Stati.jl
@@ -1,38 +1,54 @@
 module Stati
-   frek=Vector{Int}
+   frek=Array{Int,2}
    function Init(maxL::Int)
       global frek
-      frek=Vector{Int}(undef,maxL+1)
+      frek=fill(0,maxL+1,2)
    end
-   function proc(W::Matrix{Int},nV::Int,nL::Int)
+   function pre(W::Matrix{Int},nV::Int,nL::Int,const i::Int)
       global frek
-      maxw1=maximum(W[1,:]);
-      for w1 in 0:maxw1
-         maxw2=0
-         nw1=0
-         for i in 1:nV
-            W[i,1]==w1 || continue
-            nw1+=1
-            W[i,2]>0 || continue
-            frek[W[i,2]]+=1
-            maxw2=max(maxw2,W[i,2])
+      fi=view(frek,:,i);
+      Wi=view(W,:,i);
+      fii=view(frek,:,3-i);
+      Wii=view(W,:,3-i);
+
+      maxwi=0
+      for it in 1:nV
+         maxwi=max(Wi[it],maxwi)
+         fi[Wi[it]+1]+=1
+      end
+
+      for wi in 0:maxwi
+         nwi=fi[wi+1]
+         if 0==nwi 
+            continue
          end
-         if nw1>0
-            mu=0.0
-            mu2=0.0
-            for i in 1:maxw1
-               mu+=i*frek[i];
-               mu2+=i*i*frek[i];
-               frek[i]=0
-            end
-            mu/=nw1
-            mu2/=nw1
-            if mu2>0
-               println(nV," ",nL," ",w1," ","-1 ",mu," ",mu2," ",(mu*mu)/mu2)
-            else
-               println(nV," ",nL," ",w1," ","-1 ",mu," ",mu2," 1")
-            end
+
+         maxwii=0
+         for it in 1:nV
+            Wi[it]==wi || continue
+            fii[Wii[it]+1]+=1
+            maxwii=max(maxwii,Wii[it])
          end
+         mu,mu2=desc(fii,maxwii);
+         mu/=nwi
+         mu2/=nwi
+
+         print(i," ",nV," ",nL," ",wi," ",mu," ",mu2)
+         if mu2>0
+            println(" ",(mu*mu)/mu2)
+         else
+            println(" 1")
+         fi[wi+1]=0
+      end
+   end
+
+   function desc(f,nf)
+      mu=0.0
+      mu2=0.0
+      for i in 1:nf
+         mu+=i*f[i+1];
+         mu2+=i*i*f[i+1];
+         frek[i]=0
       end
    end
 end
