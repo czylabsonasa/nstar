66c7b67cf74cf5e6bc21c0bd15be1484c83865c4
Noszály Csaba <czylabson.asa@gmail.com>
2018-09-30 12:41:27 +0200

    progress

--- Dev.jl
+++ Dev.jl
@@ -1,3 +1,4 @@
+# parameters for nonuser
 module Dev
    export Itip,Iw,Imu,Imu2,InV,ILepes,IN,Ip,Iq,Ir 
    Itip,Iw,Imu,Imu2,InV,ILepes,IN,Ip,Iq,Ir=1:10
--- Main.jl
+++ Main.jl
@@ -1,5 +1,15 @@
 # controls debug messages:
 const deb=false
+function Start(s::String)
+   print(s)
+   time_ns()
+end
+function Stop(t)
+   println("done in ",(time_ns()-t)/1e9," sec\n")
+end
+
+
+t1=Start("\n\nInits...\n")
 
 include("Nstar.jl")
 using .Nstar
@@ -13,44 +23,42 @@ using .Out
 include("User.jl")
 using .User:ism,chkpts
 
-
-
 Nstar.Init()
 Process.Init(Nstar.maxL,Out.Write)
 Out.Init()
 
+Stop(t1)
+
+
 # uncomment the next row and adjust the parameter to get a "deterministic" graph
 # srand( 1080 )
 
-# println("nV=",Nstar.nV)
+t1=Start("\n\nGeneration...\n")
 sbeg=time_ns()
 for rep in 1:ism
    beg=time_ns()
    Nstar.Step1()
    psiz=1
-   print(rep,": ")
+   t2=Start(" Ism=$(rep)...")
    for siz in chkpts
-      print(" (",siz)
       Nstar.Step(psiz,siz) # steps: from psiz+1 to siz
       Process.Proc(Nstar.W,Nstar.nV,siz,1)
       Process.Proc(Nstar.W,Nstar.nV,siz,2)
       psiz=siz
-      print(") ")
    end
-   println(".........",(time_ns()-beg)/1e9," sec")
+   Stop(t2)
 end
 
 Out.Finish()
 
-println("Sim: ",(time_ns()-sbeg)/1e9," sec")
+Stop(t1)
 
 
-sbeg=time_ns()
-print("Plotting...")
+t1=Start("\n\nPlotting...")
 include("Rajz.jl")
 Rajz.Init()
 Rajz.AllPlot()
-println("done in ",(time_ns()-sbeg)/1e9," sec")
+Stop(t1)
 
 
 
